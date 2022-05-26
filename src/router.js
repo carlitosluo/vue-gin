cb3ccc2025ab8db8e6f87e477564c53e76897aa1
carlistosluo <carlistos.luo@outlook.com>
2022-05-27 01:28:04 +0800

    项目初始化

--- src/router.js
+++ src/router.js
@@ -21,5 +21,16 @@ export default new Router({
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
     },
+    {
+      path: '/register',
+      name: 'register',
+      // 异步惰性加载，不用打开应用就加载
+      component: () => import('./views/register/Register.vue'),
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: () => import('./views/login/Login.vue'),
+    },
   ],
 });
---
+++ src/views/register/Register.vue
@@ -0,0 +1,67 @@
+<!--  -->
+<template>
+  <div class="register">
+    <b-row class="mt-5">
+      <b-col
+        md="8"
+        offset-md="2"
+        lg="6"
+        offset-lg="3"
+      >
+        <b-card title="注册">
+          <b-form>
+            <b-form-group label="姓名">
+              <b-form-input
+                v-model="user.name"
+                type="text"
+                placeholder="请输入用户名（选填）"
+              ></b-form-input>
+            </b-form-group>
+            <b-form-group label="手机号">
+              <b-form-input
+                v-model="user.telephone"
+                type="number"
+                placeholder="请输入手机号（选填）"
+              ></b-form-input>
+            </b-form-group>
+            <b-form-group label="密码">
+              <b-form-input
+                v-model="user.password"
+                type="password"
+                placeholder="请输入密码（选填）"
+              ></b-form-input>
+            </b-form-group>
+            <b-form-group>
+              <b-button
+                variant="outline-primary"
+                block
+              >注册</b-button>
+            </b-form-group>
+          </b-form>
+        </b-card>
+      </b-col>
+    </b-row>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      user: {
+        name: '',
+        telephone: '',
+        password: '',
+      },
+    };
+  },
+
+  methods: {
+    register() {
+      console.log('re');
+    },
+  },
+};
+</script>
+<style lang='scss' scoped>
+</style>
